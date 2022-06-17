e4481eac406ae3f9d1c2c11e6e6e528823b2072a
洪崧瑋 <apple2004tay@gmail.com>
2022-06-17 12:24:24 +0800

    設定Export相關檔案

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,8 +5,10 @@ var _radius = 0
 
 var _start_motion_pos : Vector2
 var _end_motion_Pos : Vector2
-var direction : Vector2
-var distance
+var _direction : Vector2
+var _distance
+
+export var power: int = 5
 
 func _ready():
 	_radius = _collision.shape.radius
@@ -19,14 +21,14 @@ func _input(event):
 			sleeping = true
 		elif !event.pressed && sleeping:
 			sleeping = false
-			var force = distance * 5
-			apply_central_impulse(direction * force)
+			var force = _distance * power
+			apply_central_impulse(_direction * force)
 			update()
 			
 	if event is InputEventMouseMotion && sleeping:
 		_end_motion_Pos = get_global_mouse_position()
-		direction = _end_motion_Pos.direction_to(_start_motion_pos)
-		distance = min(_end_motion_Pos.distance_to(_start_motion_pos), 300)
+		_direction = _end_motion_Pos.direction_to(_start_motion_pos)
+		_distance = min(_end_motion_Pos.distance_to(_start_motion_pos), 300)
 		update()
 	
 func _integrate_forces(state):
@@ -36,4 +38,4 @@ func _draw():
 	if sleeping:
 		#draw_circle(_start_motion_pos - position, 15, Color.red)
 		#draw_line(_start_motion_pos - position, _end_motion_Pos - position, Color.red, 2)
-		draw_line(Vector2.ZERO, Vector2(1, 1) * distance * direction, Color.red, 5)
+		draw_line(Vector2.ZERO, Vector2(1, 1) * _distance * _direction, Color.red, 5)

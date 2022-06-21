9099a843e58766bb61d268501fd5edb90460ecc0
洪崧瑋 <apple2004tay@gmail.com>
2022-06-21 15:18:02 +0800

    1.調整Level3關卡設置 2.調整Player的linear_damp 3.新增Player可操作時的提示 4.調整Player操作的判斷方式及新增手勢拉動提示

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,41 +1,54 @@
 extends RigidBody2D
 
 onready var _collision = get_node("CollisionShape2D")
-var _radius = 0
+onready var _stopping_tip : Sprite = get_node("StoppingTip")
 
 var _start_motion_pos : Vector2
 var _end_motion_Pos : Vector2
 var _direction : Vector2
 var _distance
+var _max_distance = 400
 
-export var power: int = 5
+var _stopping : bool
+var _dragging : bool
 
-func _ready():
-	_radius = _collision.shape.radius
+export var power : int = 5
+
+func _physics_process(delta):
+	if linear_velocity.length() < 30:
+		_stopping = true
+	else:
+		_stopping = false
+	
+	_stopping_tip.visible = _stopping
 	
 func _input(event):
 	if event is InputEventMouseButton:
 		var pos = get_global_mouse_position()
-		if event.pressed && linear_velocity.length() < 30:
+		
+		if event.pressed && _stopping:
 			_start_motion_pos = pos
 			sleeping = true
-		elif !event.pressed && sleeping:
+			_dragging = true
+		elif !event.pressed && _dragging:
 			sleeping = false
+			_dragging = false
 			var force = _distance * power
 			apply_central_impulse(_direction * force)
 			update()
 			
-	if event is InputEventMouseMotion && sleeping:
+	if event is InputEventMouseMotion && _dragging:
 		_end_motion_Pos = get_global_mouse_position()
 		_direction = _end_motion_Pos.direction_to(_start_motion_pos)
-		_distance = min(_end_motion_Pos.distance_to(_start_motion_pos), 300)
+		_distance = min(_end_motion_Pos.distance_to(_start_motion_pos), _max_distance)
 		update()
 	
 func _integrate_forces(state):
 	rotation_degrees = 0
 	
 func _draw():
-	if sleeping:
-		#draw_circle(_start_motion_pos - position, 15, Color.red)
-		#draw_line(_start_motion_pos - position, _end_motion_Pos - position, Color.red, 2)
-		draw_line(Vector2.ZERO, Vector2(1, 1) * _distance * _direction, Color.red, 5)
+	if _dragging:
+		draw_circle(_start_motion_pos - position, 15, Color.white)
+		draw_line(_start_motion_pos - position, _end_motion_Pos - position, Color.white, 5)
+		
+		draw_line(Vector2.ZERO, Vector2(1, 1) * _distance * _direction, Color.dodgerblue, 3)
